f76f4a29720a19f33d553db4b19d353b4407df91
Alejandro Chaves <chavesfallasa@gmail.com>
2026-04-04 16:25:00 -0600

    Avances en ventana principal
    -Boton About
    -Ingresar y aceptar usuario

--- Ventana principal.py
+++ Ventana principal.py
@@ -1,5 +1,30 @@
 import tkinter as tk
 window=tk.Tk()
-window.geometry('1520x680')
+window.geometry('700x700')
 window.title('Las Aventuras del estudiante')
+def botonabout():
+    venab=tk.Tk()
+    venab.geometry('800x600')
+    venab.title('Sobre este juego')
+    info=tk.Label(venab,text=('Informacion Importante'), font= ('Arial',12))
+    info.pack()
+    venab.mainloop()
+about = tk.Button(window,text=('About'),font=('Papyrus',12),command=botonabout)
+about.pack()
+
+
+def prograbotonuser():
+    user=usuario.get()
+    usuariovisible.config(text=user)
+    
+usuariovisible=tk.Label(window)
+
+instruccion=tk.Label(window,text=('Introduzca su nombre'), font=('Papyrus',12,'bold'))
+instruccion.pack()
+usuario = tk.Entry()
+usuario.config(font=('Papyrus',12))
+usuario.pack()
+botonuser=tk.Button(window,text=('Aceptar'), font=('Papyrus',12),command=prograbotonuser)
+botonuser.pack()
+usuariovisible.pack()
 window.mainloop()

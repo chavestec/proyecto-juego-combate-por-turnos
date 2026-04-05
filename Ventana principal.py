21069c90388e7f56ced95c7767d5888be2148f93
Alejandro Chaves <chavesfallasa@gmail.com>
2026-04-05 17:47:01 -0600

    Add files via upload

--- Ventana principal.py
+++ Ventana principal.py
@@ -1,18 +1,12 @@
-
+import funcionabout 
 import tkinter as tk
 window=tk.Tk()
 window.geometry('700x700')
 window.title('Las Aventuras del estudiante')
 
 
-def botonabout():
-    venab=tk.Tk()
-    venab.geometry('800x600')
-    venab.title('Sobre este juego')
-    info=tk.Label(venab,text=('Informacion Importante'), font= ('Arial',12))
-    info.pack()
-    venab.mainloop()
-about = tk.Button(window,text=('About'),font=('Papyrus',12),command=botonabout)
+
+about = tk.Button(window,text=('About'),font=('Papyrus',12),command=funcionabout.botonabout)
 about.pack()
 
 

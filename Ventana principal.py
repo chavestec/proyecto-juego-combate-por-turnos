738c5ecba04ce3767b13929db2383add5eff21e0
Alejandro Chaves <chavesfallasa@gmail.com>
2026-04-04 23:11:30 -0600

    Add files via upload
    -se anade mensaje de bienvenida

--- Ventana principal.py
+++ Ventana principal.py
@@ -1,7 +1,10 @@
+
 import tkinter as tk
 window=tk.Tk()
 window.geometry('700x700')
 window.title('Las Aventuras del estudiante')
+
+
 def botonabout():
     venab=tk.Tk()
     venab.geometry('800x600')
@@ -13,18 +16,23 @@ about = tk.Button(window,text=('About'),font=('Papyrus',12),command=botonabout)
 about.pack()
 
 
-def prograbotonuser():
-    user=usuario.get()
-    usuariovisible.config(text=user)
-    
-usuariovisible=tk.Label(window)
 
 instruccion=tk.Label(window,text=('Introduzca su nombre'), font=('Papyrus',12,'bold'))
 instruccion.pack()
+def prograbotonuser():
+    user=usuario.get()
+    bienvenido.config(text=('Bienvenido'), font=('Papyrus',18))
+    usuariovisible.config(text=user,font=('Papyrus',18))
+    
+botonuser=tk.Button(window,text=('Aceptar'), font=('Papyrus',12),command=prograbotonuser)
+
 usuario = tk.Entry()
 usuario.config(font=('Papyrus',12))
 usuario.pack()
-botonuser=tk.Button(window,text=('Aceptar'), font=('Papyrus',12),command=prograbotonuser)
 botonuser.pack()
+usuariovisible=tk.Label(window)
+
+bienvenido=tk.Label(window)
+bienvenido.pack()
 usuariovisible.pack()
 window.mainloop()

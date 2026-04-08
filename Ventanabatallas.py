512d4bb88486ae284f8548cd4f4aa8ee67ae3546
Alejandro Chaves <chavesfallasa@gmail.com>
2026-04-07 22:58:10 -0600

    Add files via upload

--- Ventanabatallas.py
+++ Ventanabatallas.py
@@ -1,10 +1,21 @@
 import tkinter as tk
 def principalis():
     venjuego=tk.Tk()
-    venjuego.geometry('700x700')
+    venjuego.geometry('1500x700')
+    venjuego.resizable(False,False)
     venjuego.title('Batalla')
-    info=tk.Label(venab,text=('Juego'), font= ('Arial',12))
-    infor=tk.Label(venab,text= n, font= ('Arial',12))
+    info=tk.Label(venjuego,text=('Juego'), font= ('Arial',12))
     info.pack()
-    infor.pack()
+    def supguardado():
+        if ventanaprincipal.s==1:
+            supcarta=tk.Label(venjuego, text=('Aqui'),font=('Arial',12))
+            supcarta.pack()
+        elif(s.get())==0:
+            nosupcarta=tk.Label(venjuego, text=('Aqui no'),font=('Arial',12))
+            nosupcarta.pack()
+        else:
+            nosupcartaa=tk.Label(venjuego, text=('Aqui menos'),font=('Arial',12))
+            nosupcartaa.pack()
+        
+         
     venjuego.mainloop()

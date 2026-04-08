a5e75a390c265b60a4ef166067fce58e4bc435b8
Alejandro Chaves <chavesfallasa@gmail.com>
2026-04-07 20:45:55 -0600

    Add files via upload

--- Ventana principal.py
+++ Ventana principal.py
@@ -1,11 +1,39 @@
 import funcionabout 
 import tkinter as tk
-import Ventanabatallas
 from PIL import Image, ImageTk
+#----------------VentanaBatallas-------------------------------------------------------------------------------------------------------------------------
+def principalis():
+    venjuego=tk.Tk()
+    venjuego.geometry('1500x700')
+    venjuego.resizable(False,False)
+    venjuego.title('Batalla')
+    info=tk.Label(venjuego,text=('Juego'), font= ('Arial',12))
+    info.pack()
+    supcarta=tk.Label(venjuego, text=('Aqui'),font= ('Arial',12))
+    nosupcarta=tk.Label(venjuego, text=('Aqui no'))
+    if (s.get())==1:
+        supcarta=tk.Label(venjuego, text=('Superman'),font= ('Arial',12),fg='black')
+        supcarta.pack()
+    if (b.get())==1:
+        batcarta=tk.Label(venjuego, text=('Batman'),font= ('Arial',12),fg='black')
+        batcarta.pack()
+    if (w.get())==1:
+        Wondercarta=tk.Label(venjuego, text=('Wonder Woman'),font= ('Arial',12),fg='black')
+        Wondercarta.pack()
+    if (f.get())==1:
+        flashcarta=tk.Label(venjuego, text=('Flash'),font= ('Arial',12),fg='black')
+        flasharta.pack()      
+    venjuego.mainloop()
+#--------------------------------------------------------------------------------
+#VentanaPrincipal
+#----------------------------------------------------------------------------------------------
 window=tk.Tk()
-window.geometry('700x700')
+window.geometry('1500x700')
 window.title('Las Aventuras del estudiante')
-
+window.resizable(False,False)
+fondo=tk.PhotoImage(file='fondo.png')
+fondo=tk.Label(window,image=fondo)
+fondo.place(relheight=1, relwidth=1, x=0, y=0 )
 
 about = tk.Button(window,text=('About'),font=('Papyrus',12),command=funcionabout.botonabout)
 about.pack()
@@ -15,8 +43,12 @@ instruccion=tk.Label(window,text=('Introduzca su nombre'), font=('Papyrus',12,'b
 instruccion.pack()
 def prograbotonuser():
     user=usuario.get()
-    bienvenido.config(text=('Bienvenido'), font=('Papyrus',18))
-    usuariovisible.config(text=user,font=('Papyrus',18))
+    if user!='':
+        bienvenido.config(text=('Bienvenido'), font=('Papyrus',18))
+        usuariovisible.config(text=user,font=('Papyrus',18))
+    else:
+        ''
+    
     
 botonuser=tk.Button(window,text=('Aceptar'), font=('Papyrus',12),command=prograbotonuser)
 
@@ -27,8 +59,8 @@ botonuser.pack()
 usuariovisible=tk.Label(window)
 
 bienvenido=tk.Label(window)
-bienvenido.pack()
-usuariovisible.pack()
+bienvenido.place(x=10,y=50)
+usuariovisible.place(x=10, y=100)
 
         
 
@@ -72,22 +104,34 @@ Wobox.place(x=220,y=300)
 Batbox.place(x=10, y=415)
 Flashbox.place(x=220, y=415)
 
+
+    
 def paginabatalla():
-    if (s.get())+(w.get())+(b.get())+(f.get())==3 and (usuario.get()!=''):
-        window.destroy()
-        Ventanabatallas.principalis()
+    sumapersonajes=(s.get())+(w.get())+(b.get())+(f.get())
+    if sumapersonajes==3:
+        if (usuario.get()!=''):
+            principalis()
+        else:
+            recordatorio=tk.Label(window,text=('DEBE ESCRIBIR SU NOMBRE, NOBLE JUGADOR'), font=('Papyrus',12,'bold'), bg='Black', fg='white')
+            recordatorio.place(x=10,y=250)
+            
         
     elif(s.get())+(w.get())+(b.get())+(f.get())!=3:
-        recordatorio=tk.Label(window,text=('DEBE SELECCIONAR TRES PERSONAJE'), font=('Papyrus',12,'bold'))
-        recordatorio.pack()
+        recordatorio=tk.Label(window,text=('DEBE SELECCIONAR TRES PERSONAJES'), font=('Papyrus',12,'bold'))
+        recordatorio.place(x=10,y=250)
 
         
 iniciar=tk.Button(window, text='Iniciar', font=('Papyrus',12),command=paginabatalla)
 
 iniciar.pack()
-
-
-
 window.mainloop()
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+        
+
+
+
+
 
 
